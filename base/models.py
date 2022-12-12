a29a11eba2510b1110944107c712f2dd74c01498
omri <omr2361@gmail.com>
2022-12-12 23:16:15 +0200

    4

--- base/models.py
+++ base/models.py
@@ -1,7 +1,5 @@
-from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
-from sqlalchemy import null
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
